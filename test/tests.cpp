296e8cb06488edfd4db323611c00a64f6aab4d0f
Geoffrey Hunter <gbmhunter@gmail.com>
2024-12-26 21:14:46 +1300

    GMock now working.

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <gmock/gmock.h>
+
 #include "NinjaHsm.hpp"
 
 class TestEvent {
@@ -17,31 +19,61 @@ public:
         std::bind(&TestHsm::State1_Event, this),
         std::bind(&TestHsm::State1_Exit, this),
         nullptr
+      ),
+      state2(
+        "State2",
+        std::bind(&TestHsm::State2_Entry, this),
+        std::bind(&TestHsm::State2_Event, this),
+        std::bind(&TestHsm::State2_Exit, this),
+        nullptr
       ) {
       addState(&state1);
+      addState(&state2);
     }
 
     NinjaHsm::State state1;
+    NinjaHsm::State state2;
 
 private:
 
-  void State1_Entry() {
-    std::cout << "State1_Entry" << std::endl;
-  }
+    virtual void State1_Entry() {
+        std::cout << "State1_Entry" << std::endl;
+    }
 
-  void State1_Event() {
-    std::cout << "State1_Event" << std::endl;
-  }
+    virtual void State1_Event() {
+        std::cout << "State1_Event" << std::endl;
+    }
 
-  void State1_Exit() {
-    std::cout << "State1_Exit" << std::endl;
-  }
+    virtual void State1_Exit() {
+        std::cout << "State1_Exit" << std::endl;
+    }
+
+    virtual void State2_Entry() {
+        std::cout << "State2_Entry" << std::endl;
+    }
+
+    virtual void State2_Event() {
+        std::cout << "State2_Event" << std::endl;
+    }
+
+    virtual void State2_Exit() {
+        std::cout << "State2_Exit" << std::endl;
+    }
+};
+
+class MockTestHsm : public TestHsm {
+public:
+    MOCK_METHOD0(State1_Entry, void());
+    MOCK_METHOD0(State1_Event, void());
+    MOCK_METHOD0(State1_Exit, void());
 };
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
     // Create test HSM
-    TestHsm hsm;
+    MockTestHsm hsm;
+
+    EXPECT_CALL(hsm, State1_Entry());
 
     // Transition to state
     hsm.initialTransitionTo(&hsm.state1);
