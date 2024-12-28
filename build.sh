b1c00d11cbc0ab7a8a68ce8c4804759a430ea2be
Geoffrey Hunter <gbmhunter@gmail.com>
2024-12-28 21:48:07 +1300

    Change CMake project name to NinjaHSM.

--- build.sh
+++ build.sh
@@ -10,3 +10,10 @@ make
 # Run tests
 # ./test/hello_test --gtest_filter="HsmTests.EventsBubbleUpToParentStates"
 ./test/standard/hello_test
+
+cd ../test/cmake_fetch_content
+mkdir -p build
+cd build
+cmake ..
+cmake --build .
+./cmake_fetch_content
