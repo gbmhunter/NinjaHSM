6d12484d90138f49ad9f6c370afd94b7849b9a18
Geoffrey Hunter <gbmhunter@gmail.com>
2024-12-31 16:07:20 +1300

    Removed cmake_fetch_content test as the example tests that for us.

--- build.sh
+++ build.sh
@@ -12,14 +12,6 @@ make
 # Run tests
 # ./test/standard/hello_test --gtest_filter="HsmTests.CanTransitionToChildStateFromParentEntry"
 ./test/standard/hello_test
-# exit 0
-
-cd $SCRIPT_DIR/test/cmake_fetch_content
-mkdir -p build
-cd build
-cmake ..
-cmake --build .
-./cmake_fetch_content
 
 # Build the basic example
 cd $SCRIPT_DIR/examples/basic_example
